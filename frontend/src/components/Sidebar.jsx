cad2f6f85b2e0362690e8ef5f5d318612bd7ee20
prashantsingh019 <ps731278@gmail.com>
2024-12-25 17:06:42 +0530

    conditional rendering implementation

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,32 +9,32 @@ import subscription from "../assets/youtube-subscription.svg"
 import playlist from "../assets/playlist_play_icon.svg"
 import like from "../assets/like.svg"
 import watchlater from "../assets/watch-later.svg"
-const Sidebar = () => {
+const Sidebar = ({visiblity}) => {
   return (
-    <div id='sidebar'>
+    <div className={`sidebar ${visiblity?'open':''}`}>
        <div className="top">
-        <div className='sidebar-option' ><FontAwesomeIcon icon={faHouse} /> Home</div>
-        <div className='sidebar-option' >
+        <div className={`sidebar-option ${visiblity?'close':''}`}><FontAwesomeIcon icon={faHouse} /> Home</div>
+        <div className={`sidebar-option ${visiblity?'close':''}`} >
         <img src={shorts} className='inline-block w-5'/>    
         Shorts</div>
-        <div className='sidebar-option' > <img src={subscription} className='inline-block w-5'/>    Subscriptions</div>
+        <div className={`sidebar-option ${visiblity?'close':''}`} > <img src={subscription} className='inline-block w-5'/>    Subscriptions</div>
        </div>
-       <div className="middle top">
+       <div className={`middle ${visiblity?'hide':''}`}>
         <div>You&nbsp;&nbsp;<FontAwesomeIcon icon={faGreaterThan} className='text-sm font-thin'/></div>
         <div>
-            <div className='sidebar-option'><FontAwesomeIcon icon={faClockRotateLeft} /> History</div>
-            <div className='sidebar-option'> <img src={playlist} className='inline-block w-5'/>  Playlist</div>
-            <div className='sidebar-option'><FontAwesomeIcon icon={faCirclePlay} /> Your videos</div>
-            <div className='sidebar-option'> <img src={watchlater} className='inline-block w-5'/>  Watch Later</div>
-            <div className='sidebar-option'> <img src={like} className='inline-block w-5'/> Liked videos</div>
+            <div className={`sidebar-option ${visiblity?'close':''}`}><FontAwesomeIcon icon={faClockRotateLeft} /> History</div>
+            <div className={`sidebar-option ${visiblity?'close':''}`}> <img src={playlist} className='inline-block w-5'/>  Playlist</div>
+            <div className={`sidebar-option ${visiblity?'close':''}`}><FontAwesomeIcon icon={faCirclePlay} /> Your videos</div>
+            <div className={`sidebar-option ${visiblity?'close':''}`}> <img src={watchlater} className='inline-block w-5'/>  Watch Later</div>
+            <div className={`sidebar-option ${visiblity?'close':''}`}> <img src={like} className='inline-block w-5'/> Liked videos</div>
         </div>
        </div>
-       <div className="bottom">
+       <div className={`bottom ${visiblity?'hide':''}`}>
         <div>Subscriptions</div>
         <div>
-            <div className='sidebar-option' > <FontAwesomeIcon icon={faUser} className='rounded-full px-3 py-2 bg-gray-200"'/>Internshala</div>
-            <div className='sidebar-option' > <FontAwesomeIcon icon={faUser} className='rounded-full px-3 py-2 bg-gray-200"'/>freeCodeCamp</div>
-            <div className='sidebar-option'> <FontAwesomeIcon icon={faUser} className='rounded-full px-3 py-2 bg-gray-200"'/>ChaiWithCode</div>
+            <div className={`sidebar-option ${visiblity?'close':''} `} > <FontAwesomeIcon icon={faUser} className='rounded-full px-3 py-2 bg-gray-200"'/>Internshala</div>
+            <div className={`sidebar-option ${visiblity?'close':''} `} > <FontAwesomeIcon icon={faUser} className='rounded-full px-3 py-2 bg-gray-200"'/>freeCodeCamp</div>
+            <div className={`sidebar-option ${visiblity?'close':''} `} > <FontAwesomeIcon icon={faUser} className='rounded-full px-3 py-2 bg-gray-200"'/>ChaiWithCode</div>
         </div>
        </div>
     </div>
