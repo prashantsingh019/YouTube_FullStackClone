06b8e69aa57b47d1ac897f42e9b7b1642bcaeae7
prashantsingh019 <ps731278@gmail.com>
2024-12-26 17:06:50 +0530

    imported video changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import "./App.css"
 import Sidebar from './components/Sidebar'
 import Gallery from './components/Gallery'
+import VideoPage from './components/VideoPage'
 const App = () => {
   const [isSidebarOpen,setSidebar] = useState(true);
   const sidebar = () => {
@@ -15,7 +16,8 @@ const App = () => {
        <Navbar sidebar={sidebar}/>
        <div className="app-center flex">
          <Sidebar visiblity={isSidebarOpen}/>
-         <Gallery/>
+         {/* <Gallery/> */}
+         <VideoPage/>
        </div>    
     </div>
   )
