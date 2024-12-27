d06705045ff64e0715a132945525ab1e3bffeb41
prashantsingh019 <ps731278@gmail.com>
2024-12-27 15:10:47 +0530

    defined routing here

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {createBrowserRouter,RouterProvider} from "react-router"
+import Gallery from './components/Gallery.jsx'
+import VideoPage from './components/VideoPage.jsx'
+import UserSidebar from './components/userSidebar.jsx'
 
+
+const routes = createBrowserRouter([
+  {
+    path:'',
+    element:<App/>,
+    children:[
+      {
+        path:'',
+        element:<Gallery/>
+      },
+      {
+        path:'/watch',
+        element:<VideoPage/>
+      },
+      {
+        path:'/sidebar',
+        element:<UserSidebar/>
+      }
+
+    ]
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routes}/>
   </StrictMode>,
 )
