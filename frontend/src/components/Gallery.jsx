1c95b160e9c302c59f90f463abc49c9f2a7c69aa
prashantsingh019 <ps731278@gmail.com>
2024-12-29 00:25:16 +0530

    built login UI feature

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import Tags from './tags'
 import { videosThumbnail, youtubeTags } from '../utils/dummyData'
 import Video from './Video'
+import { useSelector } from 'react-redux'
 
 const Gallery = () => {
- 
+  const isLoggedIn = useSelector((state) => state.userLoginStatus.isSignedIn);
+  console.log(isLoggedIn,"h");
+   
   return (
-    <div>
+    <div className='w-[100%]'>
+       {isLoggedIn?( <>
        <div className="tags">
         {
             youtubeTags.map((tag) => {
@@ -14,12 +18,16 @@ const Gallery = () => {
             })
         }
       </div>
-       <div className="video-gallery flex flex-wrap gap-10 border justify-center p-[0.5rem]">
+       <div className="video-gallery gap-10 border p-[0.5rem]">
         {videosThumbnail.map((video) => {
           return  <Video data={video}/>
         })}
        </div>
-    </div>
+       </>): <div className="blank-page">
+         <h1>Try searching to get started</h1>
+         <h3 className='text-lg text-gray-700'>Start watching videos to help us build a feed of videos you'll love</h3>
+       </div>}
+      </div>
   )
 }
 
