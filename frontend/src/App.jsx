1fed080873976aa104c7615351b65d9cfed31b0f
prashantsingh019 <ps731278@gmail.com>
2024-12-29 00:24:01 +0530

    provided store

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import "./App.css"
 import Sidebar from './components/Sidebar'
 import UserSidebar from './components/userSidebar'
 import { Outlet } from 'react-router'
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 const App = () => {
   const [isSidebarOpen,setSidebar] = useState(true);
   const [isRightSidebarOpen,setRightSidebar] = useState(false);
@@ -16,14 +18,16 @@ const App = () => {
   }
   
   return (
+    <Provider store={store}>
     <div className='overflow-x-hidden'> 
        <Navbar sidebar={sidebar} rightSidebar={rightsidebar}/>
        <div className="app-center flex">
          <Sidebar visiblity={isSidebarOpen}/>
          <Outlet/>
-         <UserSidebar visiblity={isRightSidebarOpen}/>
+         <UserSidebar visiblity={isRightSidebarOpen} fn={setRightSidebar}/>
        </div>    
     </div>
+    </Provider>
   )
 }
 
