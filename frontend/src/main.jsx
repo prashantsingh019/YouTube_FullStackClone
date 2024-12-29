414fb7b225ed757253baa53592d37c61d5f86eb6
prashantsingh019 <ps731278@gmail.com>
2024-12-29 22:56:55 +0530

    some adjustments

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,8 +5,9 @@ import App from './App.jsx'
 import {createBrowserRouter,RouterProvider} from "react-router"
 import Gallery from './components/Gallery.jsx'
 import VideoPage from './components/VideoPage.jsx'
-import UserSidebar from './components/userSidebar.jsx'
 import ChannelPage from './components/ChannelPage.jsx'
+import LoginForm from './components/LoginForm.jsx'
+import UserSidebar from './components/UserSidebar.jsx'
 
 
 const routes = createBrowserRouter([
@@ -29,6 +30,10 @@ const routes = createBrowserRouter([
       {
         path:'/channel+page',
         element:<ChannelPage/>
+      },
+      {
+        path:'/login',
+        element:<LoginForm/>
       },
 
     ]
