e75f5fee2213008211083461453f1a2493f898a7
prashantsingh019 <ps731278@gmail.com>
2024-12-31 23:18:15 +0530

    did some adjustments

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,90 +1,131 @@
-import React, { useState } from 'react'
-import google from "../assets/google_color.svg"
+import React, { useState } from "react";
+import google from "../assets/google_color.svg";
 import { useNavigate } from "react-router";
-import { useDispatch } from 'react-redux';
-import {login} from "../redux/userLogged.js"
+import { useDispatch } from "react-redux";
+import { login } from "../redux/userLogged.js";
+import axios from "axios";
 const LoginForm = () => {
+  const [formType, setFormType] = useState("login");
   const dispatch = useDispatch();
-  const [formType,setFormType]= useState("login")
   const blankForm = {
-    name:'',
-    email:'',
-    password:''
-  }
-  const [formData,setFormData] = useState({
-    name:'',
-    email:'',
-    password:''
+    name: "",
+    email: "",
+    password: "",
+  };
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
   });
- const handleDataInput = (e) => {
+  const handleDataInput = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const navigate = useNavigate();
+  const handleSubmit = () => {
 
-   const {name,value} = e.target;
-   setFormData((prevData)=>({
-     ...prevData,
-     [name]:value
-   }))
+    if (
+      formData.name == "" ||
+      formData.email == "" ||
+      formData.password == ""
+    ) {
+      setFormData(blankForm);
+      alert("all fields are required");
 
- }
- const navigate = useNavigate();
- const handleSubmit = () => {
-  if(formType === 'login'){
-    formData.name = 'notrequired'
-  }
-  
-  if(formData.name == '' || formData.email == '' || formData.password == ''){
-    setFormData(blankForm)
-    alert('all fields are required')
-    
-    return
-   }
-
-  console.log(formData);
-   
-   dispatch(login(formData.name))
-   setFormData(blankForm)
-   navigate('/')
- }
+      return;
+    }
+    if (formType === "login") {
+      
+    }
+    if (formType === "signup") {
+      axios.post('http://localhost:3000/register/',formData)
+      .then((res) => console.log("user added sucessfully"))
+      .catch((err) => console.error(err))
+     }
+    dispatch(login(formData.name));
+    setFormData(blankForm);
+    navigate("/");
+  };
 
   return (
-    <div className=' w-96 p-5 flex flex-col gap-2 mx-auto mt-10'>
+    <div className=" w-96 p-5 flex flex-col gap-2 mx-auto mt-10">
       <div className="title text-3xl font-semibold">Login to your account</div>
       <div className="options flex items-center flex-col gap-2">
-        <img src={google} className='inline-block w-10 shadow-md rounded-xl'/> 
-         <div className="second-option">Or use e-mail address</div>
+        <img src={google} className="inline-block w-10 shadow-md rounded-xl" />
+        <div className="second-option">Or use e-mail address</div>
       </div>
       <div className="actual-form">
-        {
-        formType === "signup" ?  <div className="form-control">
-          <label htmlFor="fullname">Full Name</label>
-          <input type="text" name='name' className='input-field' placeholder='for ex:John' onChange={handleDataInput} value={formData.name} required/>
-      </div>:''
-        }
-       
+        {formType === "signup" ? (
+          <div className="form-control">
+            <label htmlFor="fullname">Full Name</label>
+            <input
+              type="text"
+              name="name"
+              className="input-field"
+              placeholder="for ex:John"
+              onChange={handleDataInput}
+              value={formData.name}
+              required
+            />
+          </div>
+        ) : (
+          ""
+        )}
+
         <div className="form-control">
-            <label htmlFor="email">Email</label>
-            <input type="text" name='email' className='input-field' placeholder='📧' onChange={handleDataInput} value={formData.email} required/>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            name="email"
+            className="input-field"
+            placeholder="📧"
+            onChange={handleDataInput}
+            value={formData.email}
+            required
+          />
         </div>
         <div className="form-control">
-            <label htmlFor="password">Password</label>
-            <input type="password" name='password' className='input-field' required placeholder='🔒' onChange={handleDataInput} value={formData.password}/>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            className="input-field"
+            required
+            placeholder="🔒"
+            onChange={handleDataInput}
+            value={formData.password}
+          />
         </div>
         <div className="some-options flex justify-between">
-            <span className='flex items-center gap-1'>
-                <input type="checkbox"/>
-                Remember me
-            </span>
-            <span className='inline-block p-5'>
-                <a href="/forgot">
-                 forgot password
-                </a>
-            </span>
-
+          <span className="flex items-center gap-1">
+            <input type="checkbox" />
+            Remember me
+          </span>
+          <span className="inline-block p-5">
+            <a href="/forgot">forgot password</a>
+          </span>
         </div>
-         <button className='bg-sky-700 w-[100%] text-white rounded-sm' onClick={handleSubmit}>{formType === "signup"?"Sign Up":"Login now"}</button>
-         <div className='mt-2'>Don't have a account? <span className='text-red-600 font-bold cursor-pointer' onClick={()=>setFormType("signup")}>sign up now</span></div>
+        <button
+          className="bg-sky-700 w-[100%] text-white rounded-sm"
+          onClick={handleSubmit}
+        >
+          {formType === "signup" ? "Sign Up" : "Login now"}
+        </button>
+        <div className="mt-2">
+          Don't have a account?{" "}
+          <span
+            className="text-red-600 font-bold cursor-pointer"
+            onClick={() => setFormType("signup")}
+          >
+            sign up now
+          </span>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
