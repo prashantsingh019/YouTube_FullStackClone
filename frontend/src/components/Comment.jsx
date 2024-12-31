84d48c6044194c9afc000929c3571da80f87cef9
prashantsingh019 <ps731278@gmail.com>
2024-12-31 14:38:03 +0530

    did some updates in comment component

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -6,17 +6,17 @@ import dislike from "../assets/dislike.svg"
 import {
     faUser
   } from "@fortawesome/free-solid-svg-icons";
-const Comment = () => {
+const Comment = ({data}) => {
   return (
     <div className='flex gap-3'>
       <div className="left "><FontAwesomeIcon icon={faUser} className='rounded-full px-3 py-3 bg-gray-400'/></div>
       <div className="mid flex-[8]">
         <div className="channel-handle">
-            <span>@minnah13</span>&nbsp;
+            <span>{data["user"]}</span>&nbsp;
             <span>5 days ago</span>
         </div>
         <div className="actual-comment">
-            Can you do a video on what helped you get into Google?
+           {data.content}
         </div>
         <div>
                <button><img src={like} className='inline-block w-7 border-r-gray-400 border-solid'/></button>&nbsp;
