8c781aaf4ec9dad7436aa22309a2367b08097fa3
prashantsingh019 <ps731278@gmail.com>
2024-12-31 14:32:47 +0530

    some adjustments only

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import mongoose from "mongoose";
+import { routes } from "./Routes/videoRoutes.js";
+const app = express();
+const port = 3000;
+import cors from "cors"
+app.use(express.json())
+app.use(cors())
+app.listen(port,() => {
+   console.log(`Server is currently running on port ${port}`);
+})
+
+const uri = "mongodb+srv://prakul:prakul@cluster0.qhlea.mongodb.net/Youtube_DataBase";
+mongoose.connect(uri)
+const dataBase = mongoose.connection;
+dataBase.on('open',()=>{
+    console.log("Database Connected")
+})
+dataBase.on('error',()=>{
+    console.log("Database not Connected")
+})
+
+
+routes(app)
