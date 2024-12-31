7f6d09332ee2d15881edae5350043d243b1e4c08
prashantsingh019 <ps731278@gmail.com>
2024-12-31 16:08:57 +0530

    connected client to backend

--- frontend/src/components/Gallery.jsx
+++ frontend/src/components/Gallery.jsx
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Tags from './tags'
-import { videosThumbnail, youtubeTags } from '../utils/dummyData'
+import {youtubeTags,baseUrl} from '../utils/dummyData'
 import Video from './Video'
 import { useSelector } from 'react-redux'
+import axios from "axios"
 
 const Gallery = () => {
+  const [data,setData] = useState([]);
   const isLoggedIn = useSelector((state) => state.userLoginStatus.isSignedIn);
   console.log(isLoggedIn,"h");
-   
+  useEffect(()=>{
+    axios.get("http://localhost:3000/api")
+    .then((res) => setData(res.data))
+    .catch((error) => console.error(error))
+  },[])
+
   return (
     <div className='w-[100%]'>
        {isLoggedIn?( <>
@@ -19,8 +26,8 @@ const Gallery = () => {
         }
       </div>
        <div className="video-gallery gap-10 border p-[0.5rem]">
-        {videosThumbnail.map((video) => {
-          return  <Video data={video}/>
+        {data.map((video) => {
+          return  <Video data={video} baseUrl={baseUrl}/>
         })}
        </div>
        </>): <div className="blank-page">
