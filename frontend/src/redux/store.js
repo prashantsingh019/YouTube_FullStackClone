ccfc7b2873bbaa4008722503ffa6aba0e335d208
prashantsingh019 <ps731278@gmail.com>
2024-12-31 14:35:56 +0530

    built currentUserSlice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,10 @@
-import { configureStore, createSlice } from '@reduxjs/toolkit';
+import { configureStore, createSlice, current } from '@reduxjs/toolkit';
 import { authSlice } from './authSlice.js';
-
+import { userLogin } from './userLogged.js';
 export const store = configureStore({
     reducer:{
        userLoginStatus:authSlice.reducer,
+       currentUser:userLogin.reducer,
        }
 })
 
